9c56052c0b2e4c5cb3eb66345705796919a7eeae
ZzRanger <calebmarklim@gmail.com>
2020-10-10 21:09:02 -0500

    merged files

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
-const config = {
-    apiKey: "AIzaSyBdyAmQjUPwUswHQidFoM85y3LJjp3dkpg",
-    authDomain: "double-check-3cdcc.firebaseapp.com",
-    databaseURL: "https://double-check-3cdcc.firebaseio.com"
-  };
-  firebase.initializeApp(config);
-  export const auth = firebase.auth;
-  export const db = firebase.database();
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCXZ3IgNwq1vczQYOoL_c0cph91RXxnKTw",
+    authDomain: "testing-500d7.firebaseapp.com",
+    databaseURL: "https://testing-500d7.firebaseio.com",
+    projectId: "testing-500d7",
+    storageBucket: "testing-500d7.appspot.com",
+    messagingSenderId: "583234649390",
+    appId: "1:583234649390:web:ecfdc4fc6ee257162b8ebf",
+    measurementId: "G-X522CEBVVE"
+});
+
+const db = firebaseApp.firestore();
+
+export {db};
