765394861b29f68fb88c1c9d9b37d8ba92b526a2
smellslikekeenspirit <pdn3628@rit.edu>
2020-10-10 21:29:37 -0400

    removed form, fresh slate

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,25 +19,8 @@ export default class HomePage extends React.Component {
 					<mark> make sure you don't leave class confused </mark>
 				</h1>
                 <div>
-        <form onSubmit={this.handleSubmit}>
-          <h1>
-            Sign Up to
-          <Link to="/">Chatty</Link>
-          </h1>
-          <p>Fill in the form below to create an account.</p>
-          <div>
-            <input placeholder="Email" name="email" type="email" onChange={this.handleChange} value={this.state.email}></input>
-          </div>
-          <div>
-            <input placeholder="Password" name="password" onChange={this.handleChange} value={this.state.password} type="password"></input>
-          </div>
-          <div>
-            {this.state.error ? <p>{this.state.error}</p> : null}
-            <button type="submit">Sign up</button>
-          </div>
-          <hr></hr>
-          <p>Already have an account? <Link to="/login">Login</Link></p>
-        </form>
+                    <div>
+                    </div>
       </div>
 				<button className="button" onClick={this.handleClick}>Log in</button>
 			</div>
